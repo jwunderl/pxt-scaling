8dddd555a47198a9ce4e5baba0abf93a4094b9e2
Joey Wunderlich <jwunderl@users.noreply.github.com>
2020-03-26 19:08:26 -0700

    scale2x and scale3x in js; maybe move to cpp

--- test.ts
+++ test.ts
@@ -1,1 +1,29 @@
 // tests go here; this will not be compiled when this package is used as an extension.
+let mySprite = sprites.create(img`
+    . . . . . . . 6 . . . . . . . .
+    . . . . . . 8 6 6 . . . 6 8 . .
+    . . . e e e 8 8 6 6 . 6 7 8 . .
+    . . e 2 2 2 2 e 8 6 6 7 6 . . .
+    . e 2 2 4 4 2 7 7 7 7 7 8 6 . .
+    . e 2 4 4 2 6 7 7 7 6 7 6 8 8 .
+    e 2 4 5 2 2 6 7 7 6 2 7 7 6 . .
+    e 2 4 4 2 2 6 7 6 2 2 6 7 7 6 .
+    e 2 4 2 2 2 6 6 2 2 2 e 7 7 6 .
+    e 2 4 2 2 4 2 2 2 4 2 2 e 7 6 .
+    e 2 4 2 2 2 2 2 2 2 2 2 e c 6 .
+    e 2 2 2 2 2 2 2 4 e 2 e e c . .
+    e e 2 e 2 2 4 2 2 e e e c . . .
+    e e e e 2 e 2 2 e e e c . . . .
+    e e e 2 e e c e c c c . . . . .
+    . c c c c c c c . . . . . . . .
+`, SpriteKind.Player)
+
+mySprite.x -= 30;
+mySprite.y -= 40;
+
+const scale2xSprite = sprites.create(images.scale2x(mySprite.image))
+scale2xSprite.x += 30
+scale2xSprite.y -= 40;
+
+const scale3xSprite = sprites.create(images.scale3x(mySprite.image))
+scale3xSprite.y += 25;
