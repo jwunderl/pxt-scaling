a45ae5ba672ad6b84611ab04f6e6921932a1dc10
Joey Wunderlich <jwunderl@users.noreply.github.com>
2020-11-06 15:00:48 -0800

    expose padding, add 'create rot array' block

--- rot.ts
+++ rot.ts
@@ -1,26 +1,50 @@
-
 namespace scaling {
 
-
-    //% blockId=scalingextrot block="rotate $im by $deg degrees"
+    //% blockId=scalingextrot block="rotate $im by $deg degrees || with $padding"
     //% im.shadow=variables_get
     //% deg.defl=90
     //% group="Scaling" weight=70
     export function rot(im: Image, deg: number, padding?: number) {
         const rangeForExact = 3;
         deg = deg < 0 ? 360 - Math.abs(deg % 360) : Math.abs(deg % 360);
-        if (Math.abs(deg - 90) < rangeForExact) {
-            return rot90(im);
-        } else if (Math.abs(deg - 180) < rangeForExact) {
-            return rot90(rot90(im));
-        } else if (Math.abs(deg - 270) < rangeForExact) {
-            return rot90(rot90(rot90(im)));
-        } else if (deg < rangeForExact) {
-            return im.clone();
-        } else {
-            // todo rotsprite for non square rots
-            return rotSprite(im, deg, padding)
+
+        let base = im;
+        if (padding > 0) {
+            base = image.create(
+                im.width + (padding << 1),
+                im.height + (padding << 1)
+            );
+            base.drawImage(im, padding, padding);
         }
+
+        if (Math.abs(deg - 90) < rangeForExact) {
+            return rot90(base);
+        } else if (Math.abs(deg - 180) < rangeForExact) {
+            return rot90(rot90(base));
+        } else if (Math.abs(deg - 270) < rangeForExact) {
+            return rot90(rot90(rot90(base)));
+        } else if (deg < rangeForExact) {
+            return base === im ? base.clone() : base;
+        } else {
+            return rotSprite(base, deg)
+        }
+    }
+
+    //% blockId=scalingextrotarray block="array of $count rotations of $im|| with $padding padding"
+    //% im.shadow=variables_get
+    //% count.defl=8
+    //% padding.defl=4
+    //% group="Scaling" weight=60
+    export function createRotations(im: Image, count: number, padding = 4): Image[] {
+        const output = [];
+        for (let i = 0; i < count; ++i) {
+            output[i] = scaling.rot(
+                im,
+                i * 360 / count,
+                padding
+            );
+        }   
+        return output;
     }
 
     function rot90(im: Image) {
@@ -46,15 +70,8 @@ namespace scaling {
      * roughly based on https://en.m.wikipedia.org/wiki/Pixel-art_scaling_algorithms#RotSprite,
      * need to follow it a bit more closely
      */
-    function rotSprite(im: Image, deg: number, padding?: number) {
+    function rotSprite(im: Image, deg: number) {
         let base = im;
-        if (padding > 0) {
-            base = image.create(
-                im.width + (padding << 1),
-                im.height + (padding << 1)
-            );
-            base.drawImage(im, padding, padding);
-        }
         const scaled = scaling.scale3x(base);
         const output = image.create(scaled.width, scaled.height);
         const angleAsRadian = (deg / 180) * Math.PI;
